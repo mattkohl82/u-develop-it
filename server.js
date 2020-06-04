77839c6883e6f821a6f3f49357cf886cf2f4ee35
mattkohl82 <mattkohl82@gmail.com>
2020-06-04 15:46:19 -0500

    add post route

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 const sqlite3 = require('sqlite3').verbose();
-
 const express = require('express');
+const inputCheck = require('./utils/inputCheck');
 
 const PORT = process.env.PORT || 3001;
 const app = express();
@@ -53,6 +53,19 @@ app.get('/api/candidate/:id', (req, res) => {
     });
   });
 
+  // Create a candidate
+const sql = `INSERT INTO candidates (id, first_name, last_name, industry_connected) 
+VALUES (?,?,?,?)`;
+const params = [1, 'Ronald', 'Firbank', 1];
+// ES5 function, not arrow function, to use this
+db.run(sql, params, function(err, result) {
+if (err) {
+console.log(err);
+}
+console.log(result, this.lastID);
+});
+
+
 // Delete a candidate
 app.delete('/api/candidate/:id', (req, res) => {
     const sql = `DELETE FROM candidates WHERE id = ?`;
@@ -71,16 +84,29 @@ app.delete('/api/candidate/:id', (req, res) => {
   });
 
 // Create a candidate
-const sql = `INSERT INTO candidates (id, first_name, last_name, industry_connected) 
-              VALUES (?,?,?,?)`;
-const params = [1, 'Ronald', 'Firbank', 1];
-// ES5 function, not arrow function, to use this
-db.run(sql, params, function(err, result) {
-  if (err) {
-    console.log(err);
-  }
-  console.log(result, this.lastID);
-});
+app.post('/api/candidate', ({ body }, res) => {
+    const errors = inputCheck(body, 'first_name', 'last_name', 'industry_connected');
+    if (errors) {
+      res.status(400).json({ error: errors });
+      return;
+    }
+    const sql = `INSERT INTO candidates (first_name, last_name, industry_connected) 
+              VALUES (?,?,?)`;
+    const params = [body.first_name, body.last_name, body.industry_connected];
+    // ES5 function, not arrow function, to use `this`
+    db.run(sql, params, function(err, result) {
+        if (err) {
+            res.status(400).json({ error: err.message });
+        return;
+    }
+
+        res.json({
+        message: 'success',
+        data: body,
+        id: this.lastID
+        });
+    });
+  });
 
 // Default response for any other requests(Not Found) Catch all
 app.use((req, res) => {
@@ -89,7 +115,7 @@ app.use((req, res) => {
 
 // Start server after DB connection
 db.on('open', () => {
-  app.listen(PORT, () => {
-    console.log(`Server running on port ${PORT}`);
+    app.listen(PORT, () => {
+      console.log(`Server running on port ${PORT}`);
+    });
   });
-});
